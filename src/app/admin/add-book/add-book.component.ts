5f68c21d64861241aaf4e61826d8421bc66d81aa
Laklalech Oussama <olaklalech@SQLI756-8.SQLI>
2018-03-26 11:40:17 +0200

    add example of Constants : closes #4

--- src/app/admin/add-book/add-book.component.ts
+++ src/app/admin/add-book/add-book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Book} from '../../shared/book';
 import {BookService} from '../../shared/book.service';
 import {Router} from '@angular/router';
+import {AppConstants} from "../../shared/constants/AppConstants";
 
 @Component({
   selector: 'app-add-book',
@@ -21,5 +22,9 @@ export class AddBookComponent implements OnInit {
     this.bookService.addBook(this.book).subscribe(() => {
       this.router.navigate(['/consultation']);
     });
+  }
+
+  getMessageConfirmation() {
+    return AppConstants.DEFAULT_MSG_OK;
   }
 }
---
+++ src/app/shared/constants/AppConstants.ts
@@ -0,0 +1,6 @@
+export class AppConstants {
+  /* LES MESSAGES D'ERREUR */
+  static readonly DEFAULT_ERROR = 'Erreur : ceci est une erreur';
+  static readonly DEFAULT_WARNING = 'Attention: ceci est un warning';
+  static readonly DEFAULT_MSG_OK = 'Félicitations : action ok';
+}
